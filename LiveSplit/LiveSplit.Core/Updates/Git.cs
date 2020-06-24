48c64fdf8bde143aba32554b0a350dbb8ccb3673
Redire <remag91@gmail.com>
2020-06-24 17:57:33 +0300

    Fix startup issue when compiling from the beta1 tag.

--- LiveSplit/LiveSplit.Core/Updates/Git.cs
+++ LiveSplit/LiveSplit.Core/Updates/Git.cs
@@ -9,7 +9,7 @@ namespace LiveSplit.Updates
         public static readonly string Describe = GitInfo.version.Replace("\r", "").Replace("\n", "");
         private static readonly string[] DescribeSplit = Describe.Split('-');
         public static readonly bool IsDirty = DescribeSplit.Last() == "dirty";
-        public static readonly string LastTag = DescribeSplit[0];
+        public static readonly string LastTag = tryParseLastTag();
         public static readonly int CommitsSinceLastTag = tryParseCommitCount();
         public static readonly string Version = new[] { LastTag }
             .Concat(CommitsSinceLastTag > 0 ? new[] { CommitsSinceLastTag.ToString() } : new string[0])
@@ -20,12 +20,20 @@ namespace LiveSplit.Updates
             .Aggregate((a, b) => a + "-" + b);
         public static readonly string Branch = GitInfo.branch.Replace("\r", "").Replace("\n", "");
         public static readonly Uri RevisionUri = new Uri("https://github.com/LiveSplit/LiveSplit/tree/" + (CommitsSinceLastTag > 0 ? Revision : LastTag));
+        private static string tryParseLastTag()
+        {
+            if( DescribeSplit[0].StartsWith( "v." ) )
+            {
+                return DescribeSplit[0].Substring( 2 );
+            } else
+            {
+                return DescribeSplit[0];
+            }
+        }
         private static int tryParseCommitCount()
         {
             int result;
-            return int.TryParse(DescribeSplit[1], out result)
-                ? result
-                : 0;
+            return int.TryParse(DescribeSplit[1], out result) ? result : 1;
         }
     }
 }
